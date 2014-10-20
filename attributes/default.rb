2bf18baf271bc39b24c9d05e3bd46833bd7d76ce
Victor Iglesias <viglesiasce@gmail.com>
2014-10-20 01:49:51 -0700

    Set git repo to http address

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['java']['jdk_version'] = '7'
 default['deployer']['chef-server-url'] = 'https://10.111.4.101'
 default['deployer']['default-password'] = 'foobar'
-default['eutester']['git-repository'] = 'git@git.qa1.eucalyptus-systems.com:qa-repos/eutester.git'
+default['eutester']['git-repository'] = 'http://git.qa1.eucalyptus-systems.com/qa-repos/eutester.git'
 default['eutester']['git-revision'] = 'maint/4.0/testing'
 default['EucaTest']['git-repository'] = 'https://github.com/viglesiasce/EucaTest.git'
 default['EucaTest']['git-revision'] = 'master'
