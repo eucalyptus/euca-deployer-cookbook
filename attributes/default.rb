0805a9791763c2b54d4885d79e59aeadba775714
Vic Iglesias <vic.iglesias@hp.com>
2014-12-05 00:13:21 -0800

    Use http url for chef-serverx

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['java']['jdk_version'] = '7'
-default['deployer']['chef-server-url'] = 'https://10.111.4.101'
+default['deployer']['chef-server-url'] = 'http://10.111.4.101'
 default['deployer']['default-password'] = 'foobar'
 default['eutester']['git-repository'] = 'http://git.qa1.eucalyptus-systems.com/qa-repos/eutester.git'
 default['eutester']['git-revision'] = 'maint/4.0/testing'
