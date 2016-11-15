91197663800f568b1ace6a5372c94c2abe988c16
Tony Beckham <tony.beckham@gmail.com>
2016-11-15 14:10:01 -0800

    Use JDK8 as the default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['java']['jdk_version'] = '7'
+default['java']['jdk_version'] = '8'
 default['deployer']['chef-server-url'] = 'http://10.111.4.101'
 default['deployer']['default-password'] = 'foobar'
 default['eutester']['git-repository'] = 'http://git.qa1.eucalyptus-systems.com/qa-repos/eutester.git'
